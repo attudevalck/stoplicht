4ab31dc85b46cc3c57a9a4d69f4ef1a30c2cc55f
attudevalck <maximknockaert@gmail.com>
2025-01-27 14:08:31 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
-        basic.pause(1000)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.Off)
+        basic.pause(100)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.On)
         basic.pause(4500)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Green, Kitronik_STOPbit.DisplayLights.Off)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.On)
         basic.pause(2000)
+        Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Yellow, Kitronik_STOPbit.DisplayLights.Off)
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
     } else {
         Kitronik_STOPbit.trafficLightLED(Kitronik_STOPbit.LightColours.Red, Kitronik_STOPbit.DisplayLights.On)
